3e2e04af55888c017f2674f4409cb27082be392c
Vuong Xuan Duong <duongvx@solashi.com>
2020-08-18 11:02:10 +0700

    url login

--- project-training/app/Http/Controllers/UserController.php
+++ project-training/app/Http/Controllers/UserController.php
@@ -8,18 +8,16 @@ class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+
      */
     public function index()
     {
-        //
+        return view('admin.user.index');
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -41,8 +39,6 @@ class UserController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
@@ -50,9 +46,6 @@ class UserController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
@@ -64,7 +57,7 @@ class UserController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+
      */
     public function update(Request $request, $id)
     {
