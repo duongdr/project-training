1eef76361a40428697b8acdb10791b0ca9ce623e
duongdr <xuanduong.uet@gmail.om>
2020-08-21 15:59:21 +0700

    add observer and middleware

---
+++ project-training/app/Observers/UserObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        $id = $user->id;
+        Post::where('user_id',$id)->delete();
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
--- project-training/routes/web.php
+++ project-training/routes/web.php
@@ -13,39 +13,40 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::middleware('auth')->group(function () {
-    // Admin routes
-    Route::prefix('admin')->group(function () {
-        Route::get('home', 'DashboardController@index');
-        Route::prefix('user')->group(function (){
-            Route::get('/', 'UserController@index');
-            Route::get('create','UserController@create');
-            Route::post('create','UserController@store')->name('storeUser');
-            Route::get('delete/{id}','UserController@destroy');
-            Route::get('{id}', 'UserController@show');
-            Route::post('{user}', 'UserController@update');
-        });
-        Route::prefix('post')->group(function () {
-            Route::get('/','PostController@index');
-            Route::get('create','PostController@create');
-            Route::post('create','PostController@store')->name('storePost');
-            Route::get('{id}', 'PostController@show');
-            Route::post('{post}', 'PostController@update');
-            Route::get('delete/{id}','PostController@destroy');
-        });
+//Route::middleware('auth')->group(function () {
+// Admin routes
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+    Route::get('home', 'DashboardController@index');
+    Route::prefix('user')->group(function () {
+        Route::get('/', 'UserController@index');
+        Route::get('create', 'UserController@create');
+        Route::post('create', 'UserController@store')->name('storeUser');
+        Route::get('delete/{id}', 'UserController@destroy');
+        Route::get('{id}', 'UserController@show');
+        Route::post('{user}', 'UserController@update');
     });
-    Route::prefix('user')->group(function (){
-       Route::get('home','UserHomeController@index');
-        Route::prefix('post')->group(function () {
-            Route::get('/','PostController@index');
-            Route::get('create','PostController@create');
-            Route::post('create','PostController@store')->name('storePost');
-            Route::get('{id}', 'PostController@show');
-            Route::post('{post}', 'PostController@update')->name('updatePost');
-            Route::get('delete/{id}','PostController@destroy');
-        });
+    Route::prefix('post')->group(function () {
+        Route::get('/', 'PostController@index');
+        Route::get('create', 'PostController@create');
+        Route::post('create', 'PostController@store')->name('storePost');
+        Route::get('{id}', 'PostController@show');
+        Route::post('{post}', 'PostController@update');
+        Route::get('delete/{id}', 'PostController@destroy');
     });
 });
+Route::group(['prefix' => 'user', 'middleware' => 'user'], function () {
+    Route::get('home', 'UserHomeController@index');
+    Route::prefix('post')->group(function () {
+        Route::get('/', 'PostController@index');
+        Route::get('create', 'PostController@create');
+        Route::post('create', 'PostController@store')->name('storePost');
+        Route::get('{id}', 'PostController@show');
+        Route::post('{post}', 'PostController@update')->name('updatePost');
+        Route::get('delete/{id}', 'PostController@destroy');
+    });
+});
+//});
 
 Route::post('userLogin', 'AuthController@login')->name('userLogin');
 Route::get('logout', 'AuthController@logout')->name('logout');
